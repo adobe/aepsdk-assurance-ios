725bcedbe77b86bc77d10751a77aacf99e7b8573
pravin <pravinpk0707@gmail.com>
2021-05-06 18:20:21 -0700

    [MOB-14324] - ThreadSafe primitive - FIFO Queue

---
+++ AEPAssurance/Source/Primitives/ThreadSafeQueue.swift
@@ -0,0 +1,66 @@
+/*
+ Copyright 2021 Adobe. All rights reserved.
+ This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License. You may obtain a copy
+ of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software distributed under
+ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ OF ANY KIND, either express or implied. See the License for the specific language
+ governing permissions and limitations under the License.
+ */
+
+import Foundation
+
+/// Thread safe FIFO Queue for Assurance
+class ThreadSafeQueue<T> {
+    private let limit : Int;
+    private var array: [T] = []
+    private let accessQueue = DispatchQueue(label: "com.adobe.assurance.ThreadSafeQueue", attributes: .concurrent)
+    
+    /// Initializes the queue with the provide maximum capacity
+    init(withLimit  limit: Int) {
+        self.limit = limit
+    }
+    
+    /// Appends the specified element to the end of this queue.
+    /// If the queue has reached its limit then the first element of the queue is removed
+    func enqueue(newElement: T) {
+        self.accessQueue.async(flags:.barrier) {
+            self.array.append(newElement)
+            
+            if(self.limit > 0 && self.array.count > self.limit){
+                self.array.removeFirst()
+            }
+        }        
+    }
+    
+    /// Retrieves and removes the first element of this queue, or returns nil if this queue is empty.
+    func dequeue() -> T? {
+        var element : T?
+        self.accessQueue.sync(flags:.barrier) {
+            if (!array.isEmpty) {
+                element = array.removeFirst()
+            }
+        }
+        
+        return element;
+    }
+    
+    /// Returns the current size of the queue
+    func size() -> Int {
+        var size = 0
+        self.accessQueue.sync(flags:.barrier) {
+            size = array.count
+        }
+        
+        return size;
+    }
+    
+    ///Removes all of the elements from this queue.
+    func clear() {
+        self.accessQueue.async(flags:.barrier) {
+            self.array.removeAll()
+        }
+    }
+}
---
+++ AEPAssurance/UnitTests/ThreadSafeQueueTests.swift
@@ -0,0 +1,122 @@
+/*
+ Copyright 2021 Adobe. All rights reserved.
+ This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License. You may obtain a copy
+ of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software distributed under
+ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ OF ANY KIND, either express or implied. See the License for the specific language
+ governing permissions and limitations under the License.
+ */
+
+import XCTest
+@testable import AEPAssurance
+
+class ThreadSafeQueueTests: XCTestCase {
+
+    func test_enqueue() throws {
+        // setup
+        let queue = ThreadSafeQueue<String>(withLimit: 10)
+        
+        // test
+        queue.enqueue(newElement: "One")
+        
+        // verify
+        XCTAssertEqual(1, queue.size())
+    }
+    
+    func test_dequeue() throws {
+        // setup
+        let queue = ThreadSafeQueue<String>(withLimit: 10)
+        queue.enqueue(newElement: "One")
+        queue.enqueue(newElement: "Two")
+        queue.enqueue(newElement: "Three")
+        
+        // test and verify
+        XCTAssertEqual(3, queue.size())
+        XCTAssertEqual("One", queue.dequeue())
+        XCTAssertEqual("Two", queue.dequeue())
+        XCTAssertEqual("Three", queue.dequeue())
+        XCTAssertEqual(0, queue.size())
+    }
+    
+    func test_size() throws {
+        // setup
+        let queue = ThreadSafeQueue<Int>(withLimit: 1000)
+        for number in 1...100 {
+            queue.enqueue(newElement: number)
+        }
+            
+        // test and verify
+        XCTAssertEqual(100, queue.size())
+    }
+    
+    func test_clear() throws {
+        // setup
+        let queue = ThreadSafeQueue<Int>(withLimit: 10)
+        for number in 1...5 {
+            queue.enqueue(newElement: number)
+        }
+        
+        // test and verify
+        XCTAssertEqual(5, queue.size())
+        queue.clear()
+        XCTAssertEqual(0, queue.size())
+    }
+        
+    func test_limit() throws {
+        // setup
+        let queue = ThreadSafeQueue<Int>(withLimit: 5)
+        for number in 1...15 {
+            queue.enqueue(newElement: number)
+        }
+            
+        // test and verify
+        XCTAssertEqual(5, queue.size())
+        
+        // dequeueing only gets the last entered elements
+        for number in 11...15 {
+            XCTAssertEqual(number, queue.dequeue())
+        }
+    }
+    
+    func test_threadSafety() throws {
+        let queue = ThreadSafeQueue<String>(withLimit: 1000)
+        let group = DispatchGroup()
+        for _ in 0...10 {
+            
+            // Enqueue task
+            group.enter()
+            DispatchQueue.global().async {
+                let sleepVal = arc4random() % 1000
+                usleep(sleepVal)
+                queue.enqueue(newElement: "data")
+                group.leave()
+            }
+            
+            // Dequeuing task
+            group.enter()
+            DispatchQueue.global().async {
+                let sleepVal = arc4random() % 1000
+                usleep(sleepVal)
+                let _ = queue.dequeue()
+                group.leave()
+            }
+            
+            
+            // size check task
+            group.enter()
+            DispatchQueue.global().async {
+                let sleepVal = arc4random() % 1000
+                usleep(sleepVal)
+                let _ = queue.size()
+                group.leave()
+            } 
+        }
+        
+        let result = group.wait(timeout: DispatchTime.now() + 3)
+        XCTAssert(result == .success)
+    }
+
+}
