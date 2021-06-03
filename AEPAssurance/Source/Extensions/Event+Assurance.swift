33759955873f1aafbf2d4d231c6d564444ffd942
pravin <pravinpk0707@gmail.com>
2021-06-03 02:32:30 -0700

    [MOB-14190] - Handle places events

--- AEPAssurance/Source/Extensions/Event+Assurance.swift
+++ AEPAssurance/Source/Extensions/Event+Assurance.swift
@@ -15,12 +15,94 @@ import Foundation
 
 extension Event {
     // MARK: - Event Type/Source Detection
+
+    /// A boolean value that determines whether event is a sharedState change event (Regular and XDM).
     var isSharedStateEvent: Bool {
         return type == EventType.hub && source == EventSource.sharedState
     }
 
+    /// A boolean value that determines whether event is a places request content event.
+    var isAssuranceRequestContent: Bool {
+        return type == AssuranceConstants.SDKEventType.ASSURANCE && source == EventSource.requestContent
+    }
+
+    /// A boolean value that determines whether event is a places request content event.
+    var isPlacesRequestEvent: Bool {
+        return type == EventType.places && source == EventSource.requestContent
+    }
+
+    /// A boolean value that determines whether event is a places response content event.
+    var isPlacesResponseEvent: Bool {
+        return type == EventType.places && source == EventSource.responseContent
+    }
+
+    /// A boolean value that determines whether event is Places nearby POI request.
+    var isRequestNearByPOIEvent: Bool {
+        return name == AssuranceConstants.Places.EventName.REQUEST_NEARBY_POI
+    }
+
+    /// A boolean value that determines whether event is Places request reset.
+    var isRequestResetEvent: Bool {
+        return name == AssuranceConstants.Places.EventName.REQUEST_RESET
+    }
+
+    /// A boolean value that determines whether event is Places region response event. Also called as region entry/exit events.
+    var isResponseRegionEvent: Bool {
+        return name == AssuranceConstants.Places.EventName.RESPONSE_REGION_EVENT
+    }
+
+    /// A boolean value that determines whether event is Places region nearby POI response event.
+    var isResponseNearByEvent: Bool {
+        return name == AssuranceConstants.Places.EventName.RESPONSE_NEARBY_POI_EVENT && responseID == nil
+    }
+
     // MARK: - EventData values
+    /// A string representing shared state owner for shared state change events.
     var sharedStateOwner: String? {
         return data?[AssuranceConstants.EventDataKey.SHARED_STATE_OWNER] as? String
     }
+
+    /// A string representing POI count for places nearby POI request events.
+    var poiCount: String {
+        if let count = data?[AssuranceConstants.Places.EventDataKeys.COUNT] as? NSNumber {
+            return count.stringValue
+        }
+        return "-"
+    }
+
+    /// A string representing latitude for places nearby POI request events.
+    var latitude: String {
+        if let lat = data?[AssuranceConstants.Places.EventDataKeys.LATITUDE] as? Double {
+            return String(format: "%.6f", lat)
+        }
+        return "-"
+    }
+
+    /// A string representing longitude for places nearby POI request events.
+    var longitude: String {
+        if let lon = data?[AssuranceConstants.Places.EventDataKeys.LONGITUDE] as? Double {
+            return String(format: "%.6f", lon)
+        }
+        return "-"
+    }
+
+    /// A string representing region type (Entry/Exit) for places region events.
+    var regionEventType: String {
+        return data?[AssuranceConstants.Places.EventDataKeys.REGION_EVENT_TYPE] as? String ?? "-"
+    }
+
+    /// A string representing region name for places region events.
+    var regionName: String {
+        let defaultValue = "-"
+        guard let region = data?[AssuranceConstants.Places.EventDataKeys.TRIGGERING_REGION] as? [String: Any] else {
+            return defaultValue
+        }
+        return region[AssuranceConstants.Places.EventDataKeys.REGION_NAME] as? String ?? defaultValue
+    }
+
+    /// An Array containing dictionary of nearby POI details for a places nearby POI response event.
+    var nearByPOIs: [Any] {
+        return data?[AssuranceConstants.Places.EventDataKeys.NEARBY_POI] as? Array ?? []
+    }
+
 }
---
+++ AEPAssurance/Source/StatusUI/AssuranceClientLogVisibility.swift
@@ -0,0 +1,8 @@
+//
+//  AssuranceClientLogVisibility.swift
+//  AEPAssurance
+//
+//  Created by pprakash on 6/3/21.
+//
+
+import Foundation
