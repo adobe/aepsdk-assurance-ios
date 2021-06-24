004dcecef088f100098e33f00bb9ccdb6670ea1f
pravin <pravinpk0707@gmail.com>
2021-06-24 05:01:45 -0700

    [Tests] - AssuranceConnectionErrorTests

--- AEPAssurance/UnitTests/AssuranceConnectionError.swift
+++ AEPAssurance/UnitTests/AssuranceConnectionError.swift
@@ -1,8 +1,71 @@
-//
-//  AssuranceConnectionError.swift
-//  UnitTests
-//
-//  Created by pprakash on 6/24/21.
-//
+/*
+ Copyright 2021 Adobe. All rights reserved.
+ This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License. You may obtain a copy
+ of the License at http://www.apache.org/licenses/LICENSE-2.0
 
-import Foundation
+ Unless required by applicable law or agreed to in writing, software distributed under
+ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ OF ANY KIND, either express or implied. See the License for the specific language
+ governing permissions and limitations under the License.
+ */
+
+@testable import AEPAssurance
+import XCTest
+
+class AssuranceConnectionErrorTests: XCTestCase {
+
+    func test_AssuranceConnectionErrorEnum() throws {
+
+        // Generic Error
+        XCTAssertNotNil(AssuranceConnectionError.genericError.info.name)
+        XCTAssertNotNil(AssuranceConnectionError.genericError.info.description)
+        XCTAssertTrue(AssuranceConnectionError.genericError.info.shouldRetry)
+
+        // NoSessionId Error
+        XCTAssertNotNil(AssuranceConnectionError.noSessionID.info.name)
+        XCTAssertNotNil(AssuranceConnectionError.noSessionID.info.description)
+        XCTAssertFalse(AssuranceConnectionError.noSessionID.info.shouldRetry)
+
+        // No PinCode Error
+        XCTAssertNotNil(AssuranceConnectionError.noPincode.info.name)
+        XCTAssertNotNil(AssuranceConnectionError.noPincode.info.description)
+        XCTAssertTrue(AssuranceConnectionError.noPincode.info.shouldRetry)
+
+        // NoURL Error
+        XCTAssertNotNil(AssuranceConnectionError.noURL.info.name)
+        XCTAssertNotNil(AssuranceConnectionError.noURL.info.description)
+        XCTAssertFalse(AssuranceConnectionError.noURL.info.shouldRetry)
+
+        // noOrgId Error
+        XCTAssertNotNil(AssuranceConnectionError.noOrgId.info.name)
+        XCTAssertNotNil(AssuranceConnectionError.noOrgId.info.description)
+        XCTAssertFalse(AssuranceConnectionError.noOrgId.info.shouldRetry)
+
+        // orgIDMismatch Error
+        XCTAssertNotNil(AssuranceConnectionError.orgIDMismatch.info.name)
+        XCTAssertNotNil(AssuranceConnectionError.orgIDMismatch.info.description)
+        XCTAssertFalse(AssuranceConnectionError.orgIDMismatch.info.shouldRetry)
+
+        // connectionLimit Error
+        XCTAssertNotNil(AssuranceConnectionError.connectionLimit.info.name)
+        XCTAssertNotNil(AssuranceConnectionError.connectionLimit.info.description)
+        XCTAssertFalse(AssuranceConnectionError.connectionLimit.info.shouldRetry)
+
+        // eventLimit Error
+        XCTAssertNotNil(AssuranceConnectionError.eventLimit.info.name)
+        XCTAssertNotNil(AssuranceConnectionError.eventLimit.info.description)
+        XCTAssertFalse(AssuranceConnectionError.eventLimit.info.shouldRetry)
+
+        // clientError Error
+        XCTAssertNotNil(AssuranceConnectionError.clientError.info.name)
+        XCTAssertNotNil(AssuranceConnectionError.clientError.info.description)
+        XCTAssertFalse(AssuranceConnectionError.clientError.info.shouldRetry)
+
+        // userCancelled Error
+        XCTAssertNotNil(AssuranceConnectionError.userCancelled.info.name)
+        XCTAssertNotNil(AssuranceConnectionError.userCancelled.info.description)
+        XCTAssertFalse(AssuranceConnectionError.userCancelled.info.shouldRetry)
+
+    }
+}
