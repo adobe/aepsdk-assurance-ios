49da256a852019889207375684625dfe74a07f44
pravin <pravinpk0707@gmail.com>
2022-02-10 10:47:04 -0800

    Quick connect manager base class

--- AEPAssurance/Source/QuickConnect/QuickConnectManager.swift
+++ AEPAssurance/Source/QuickConnect/QuickConnectManager.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+class QuickConnectManager {
+    
+    private let parentExtension : Assurance
+    private let view : QuickConnectView
+    
+    init(assurance : Assurance) {
+        parentExtension = assurance;
+        view = QuickConnectView()
+        detectShakeGesture()
+    }
+    
+    func detectShakeGesture() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(handleShakeGesture),
+                                               name: NSNotification.Name(AssuranceConstants.QuickConnect.SHAKE_NOTIFICATION_KEY),
+                                               object: nil)
+    }
+    
+    
+    @objc private func handleShakeGesture() {
+        parentExtension.shouldProcessEvents = true
+        //parentExtension.invalidateTimer()
+        DispatchQueue.main.async {
+            //self.view?.appear()
+        }
+
+    }
+    
+}
--- AEPAssurance/Source/QuickConnect/QuickConnectView.swift
+++ AEPAssurance/Source/QuickConnect/QuickConnectView.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+import UIKit
+import AEPServices
+
+class QuickConnectView {
+    
+    
+    func show() {
+        guard let window = UIApplication.shared.assuranceGetKeyWindow() else {
+            Log.warning(label: AssuranceConstants.LOG_TAG, "QuickConnect View unable to get the keyWindow, ")
+             return
+         }
+    }
+    
+}
