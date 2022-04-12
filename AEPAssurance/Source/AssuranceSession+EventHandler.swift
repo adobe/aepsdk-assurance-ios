33dc33a2af4a547165ab80b5a028e530acc4417f
pravin <pravinpk0707@gmail.com>
2022-04-13 01:09:29 +0530

    Use presentationCode in session

--- AEPAssurance/Source/AssuranceSession+EventHandler.swift
+++ AEPAssurance/Source/AssuranceSession+EventHandler.swift
@@ -3,7 +3,6 @@
  This file is licensed to you under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License. You may obtain a copy
  of the License at http://www.apache.org/licenses/LICENSE-2.0
-
  Unless required by applicable law or agreed to in writing, software distributed under
  the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
  OF ANY KIND, either express or implied. See the License for the specific language
@@ -53,7 +52,7 @@ extension AssuranceSession {
     /// Handles the queuing and receiving of inbound Assurance session events.
     ///
     func handleInBoundEvents() {
-        inboundSource.setEventHandler(handler: {
+        inboundSource.setEventHandler(handler: { [self] in
             while self.inboundQueue.size() > 0 {
                 guard let event = self.inboundQueue.dequeue() else {
                     Log.trace(label: AssuranceConstants.LOG_TAG, "Unable to read a valid event from inbound event queue. Ignoring to process the Inbound event from the Assurance Session.")
@@ -71,9 +70,7 @@ extension AssuranceSession {
                     // 1. Remove the WebView UI and display the floating button
                     // 2. Share the Assurance shared state
                     // 3. Notify the client plugins on successful connection
-                    self.pinCodeScreen?.connectionSucceeded()
-                    self.statusUI.display()
-                    self.statusUI.updateForSocketConnected()
+                    presentation.onSessionConnected()
                     self.pluginHub.notifyPluginsOnConnect()
                     self.outboundSource.add(data: 1)
 
