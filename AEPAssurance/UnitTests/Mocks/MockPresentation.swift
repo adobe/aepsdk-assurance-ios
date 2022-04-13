30c25f78dfab085c9e0b3a44b681a39d621f4a98
pravin <pravinpk0707@gmail.com>
2022-04-14 01:26:29 +0530

    MockPresentation & Orchestrator

--- AEPAssurance/UnitTests/Mocks/MockPresentation.swift
+++ AEPAssurance/UnitTests/Mocks/MockPresentation.swift
@@ -1,8 +1,56 @@
 //
-//  MockPresentation.swift
-//  UnitTests
+// Copyright 2021 Adobe. All rights reserved.
+// This file is licensed to you under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License. You may obtain a copy
+// of the License at http://www.apache.org/licenses/LICENSE-2.0
 //
-//  Created by pprakash on 4/13/22.
+// Unless required by applicable law or agreed to in writing, software distributed under
+// the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+// OF ANY KIND, either express or implied. See the License for the specific language
+// governing permissions and limitations under the License.
 //
 
+@testable import AEPAssurance
+@testable import AEPCore
 import Foundation
+import XCTest
+
+class MockPresentation : AssurancePresentation {
+    
+    var expectation: XCTestExpectation?
+    required override  init(stateManager: AssuranceStateManager, sessionOrchestrator: AssuranceSessionOrchestrator) {
+        super.init(stateManager: stateManager, sessionOrchestrator: sessionOrchestrator)
+    }
+    
+    var onSessionConnectedCalled = false
+    override func onSessionConnected() {
+        onSessionConnectedCalled = true
+    }
+    
+    var onSessionReconnectingCalled = false
+    override func onSessionReconnecting() {
+        onSessionReconnectingCalled = true
+    }
+    
+    var onSessionDisconnectedCalled = false
+    override func onSessionDisconnected() {
+        onSessionDisconnectedCalled = true
+    }
+    
+    var onSessionConnectionErrorCalled = false
+    var onSessionConnectionErrorValue : AssuranceConnectionError?
+    override func onSessionConnectionError(error: AssuranceConnectionError) {
+        onSessionConnectionErrorCalled = true
+        onSessionConnectionErrorValue = error
+    }
+    
+    var addClientLogCalled = false
+    var addClientLogMessage: String?
+    var addClientLogVisibility: AssuranceClientLogVisibility?
+    override func addClientLog(_ message: String, visibility: AssuranceClientLogVisibility) {
+        addClientLogCalled = true
+        addClientLogMessage = message
+        addClientLogVisibility = visibility
+    }
+}
+
--- AEPAssurance/UnitTests/Mocks/MockSessionOrchestrator.swift
+++ AEPAssurance/UnitTests/Mocks/MockSessionOrchestrator.swift
@@ -1,8 +1,62 @@
 //
-//  MockSessionOrchestrator.swift
-//  UnitTests
+// Copyright 2021 Adobe. All rights reserved.
+// This file is licensed to you under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License. You may obtain a copy
+// of the License at http://www.apache.org/licenses/LICENSE-2.0
 //
-//  Created by pprakash on 4/13/22.
+// Unless required by applicable law or agreed to in writing, software distributed under
+// the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+// OF ANY KIND, either express or implied. See the License for the specific language
+// governing permissions and limitations under the License.
 //
 
 import Foundation
+@testable import AEPAssurance
+import XCTest
+
+class MockSessionOrchestrator : AssuranceSessionOrchestrator {
+    
+    var expectation: XCTestExpectation?
+    required override init(stateManager: AssuranceStateManager) {
+        super.init(stateManager: stateManager)
+    }
+
+    var createSessionCalled = false
+    override func createSession() {
+        createSessionCalled = true
+    }
+    
+    func setActiveSession(_ activeSession: AssuranceSession) {
+        session = activeSession
+    }
+
+    override func getActiveSession() -> AssuranceSession? {
+        return session
+    }
+
+    var terminateSessionCalled = false
+    override func terminateSession() {
+        terminateSessionCalled = true
+    }
+
+    var shutDownSessionCalled = false
+    override func shutDownSession() {
+        shutDownSessionCalled = true
+    }
+
+    var sendEventCalled = false
+    var sentEvent: AssuranceEvent?
+    override func sendEvent(_ assuranceEvent: AssuranceEvent) {
+        expectation?.fulfill()
+        sendEventCalled = true
+        sentEvent = assuranceEvent
+    }
+
+    // MARK: - AssurancePresentationDelegate methods
+    override func onPinConfirmation(_ url: URL) {
+    }
+
+    override func onDisconnect() {
+    }
+}
+
