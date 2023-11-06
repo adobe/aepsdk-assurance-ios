9ad589e938a6c3c21e73dbc33e9811bb366b1bcf
Christopher Hoffmann <cdhoffmann@users.noreply.github.com>
2023-11-06 10:01:32 -0700

    the launch id was mistakenly removed from the test app.

--- TestApp/AppDelegate.swift
+++ TestApp/AppDelegate.swift
@@ -35,7 +35,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         MobileCore.track(state: "Before SDK Init", data: nil)
         MobileCore.setLogLevel(.trace)
         
-        let launchID = ""
         let extensions = [AEPIdentity.Identity.self,
                           Lifecycle.self,
                           Signal.self,
@@ -52,7 +51,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         ]
         let appState = application.applicationState
         MobileCore.registerExtensions(extensions, {
-            MobileCore.configureWith(appId: launchID)
+            MobileCore.configureWith(appId: "94f571f308d5/f986c2be4925/launch-e96cdeaddea9-development")
             if appState != .background {
                 MobileCore.lifecycleStart(additionalContextData: nil)
             }
